2ddeaeb5754f6d5e7f99f522a83f213c1322cfdd
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-28 21:45:21 +0900

    DB接続先修正

--- backend/simple-crud-example/task.rb
+++ backend/simple-crud-example/task.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 require 'sinatra/activerecord'
 
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
-ActiveRecord::Base.establish_connection('sqlite3:///db/crud.db')
+ActiveRecord::Base.establish_connection(:development)
 
 class Task < ActiveRecord::Base
 end
