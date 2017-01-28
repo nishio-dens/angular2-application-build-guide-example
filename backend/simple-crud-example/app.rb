cdab92b6f91e74eaf657e927a3b76c58a38b6905
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-28 21:26:17 +0900

    require sqlite and activerecord

--- backend/simple-crud-example/app.rb
+++ backend/simple-crud-example/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './task'
 
 class Application < Sinatra::Base
 end
--- backend/simple-crud-example/task.rb
+++ backend/simple-crud-example/task.rb
@@ -1,3 +1,6 @@
+require 'sqlite3'
+require 'sinatra/activerecord'
+
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection('sqlite3:///db/crud.db')
 
