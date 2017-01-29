874a4d2188fc903bcc5bbe6ecbda2853eae0d48d
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-29 20:21:42 +0900

    [SimpleCrudExample] 一覧にタスクを表示できるようにした

--- frontend/simple-crud-example/src/app/tasks/task-list/task-list.component.ts
+++ frontend/simple-crud-example/src/app/tasks/task-list/task-list.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListComponent implements OnInit {
 
+  tasks: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.tasks = [
+      {
+        id: 1,
+        name: 'テストタスク',
+        description: 'テスト概要',
+        createdAt: '2017-01-01T00:00:00',
+        updatedAt: '2017-01-01T00:00:00'
+      },
+      {
+        id: 2,
+        name: 'テストタスク2',
+        description: 'テスト2概要',
+        createdAt: '2017-01-07T00:00:00',
+        updatedAt: '2017-01-07T00:00:00'
+      }
+    ];
   }
 
 }
