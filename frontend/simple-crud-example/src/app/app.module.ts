dbb6f97808d9abc629f71a3b181112d2df664eb0
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-29 18:18:50 +0900

    [SimpleCrudExample] Top Route追加

--- frontend/simple-crud-example/src/app/app.module.ts
+++ frontend/simple-crud-example/src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     FormsModule,
     HttpModule
