18224b3726ec822af8c85a1d0d4d9b7a213d8830
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-29 19:30:13 +0900

    [SimpleCrudExample] task crud定義

--- frontend/simple-crud-example/src/app/tasks/tasks-routing.module.ts
+++ frontend/simple-crud-example/src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TasksComponent} from './tasks.component';
+import {TaskListComponent} from './task-list/task-list.component';
+import {TaskDetailComponent} from './task-detail/task-detail.component';
+import {TaskNewComponent} from './task-new/task-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TasksComponent,
+    children: [
+      {
+        path: '',
+        component: TaskListComponent
+      },
+      {
+        path: 'tasks',
+        component: TaskListComponent
+      },
+      {
+        path: 'tasks/:id',
+        component: TaskDetailComponent
+      },
+      {
+        path: 'tasks/new',
+        component: TaskNewComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
