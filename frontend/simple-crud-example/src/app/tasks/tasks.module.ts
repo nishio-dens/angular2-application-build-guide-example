be7a4e65ee2374be8d597227193b9c5559e7ea61
Shinsuke Nishio <nishio@densan-labs.net>
2017-01-29 19:24:51 +0900

    [SimpleCrudExample] ng g component ./tasks/task-list / task-detail / task-new

--- frontend/simple-crud-example/src/app/tasks/tasks.module.ts
+++ frontend/simple-crud-example/src/app/tasks/tasks.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskNewComponent } from './task-new/task-new.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TasksRoutingModule
   ],
-  declarations: [TasksComponent]
+  declarations: [TasksComponent, TaskListComponent, TaskNewComponent, TaskDetailComponent]
 })
 export class TasksModule { }
