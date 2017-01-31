f843325de6f2518b11b1672b49546b3f07db3e10
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-01 02:42:16 +0900

    [SimpleCrudExample] タスクを更新できるようにした

--- frontend/simple-crud-example/src/app/tasks/task-detail/task-detail.component.ts
+++ frontend/simple-crud-example/src/app/tasks/task-detail/task-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
+import {TaskService} from '../shared/task.service';
+import {Task} from '../shared/task.model';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-task-detail',
@@ -8,10 +11,14 @@ import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
 })
 export class TaskDetailComponent implements OnInit {
 
+  task: Task = new Task();
   taskForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {
     this.taskForm = this.formBuilder.group({
       'name': ['', Validators.compose([Validators.required, Validators.maxLength(255)])],
@@ -20,10 +27,27 @@ export class TaskDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.taskService.getTask(id).subscribe(
+        (t: Task) => this.task = t,
+        _ => {
+          alert("Record Not Found");
+          this.router.navigate(['/tasks']);
+        }
+      );
+    });
   }
 
-  onSubmit(values: any) {
-    console.log(values);
+  onSubmit() {
+    this.taskService.updateTask(this.task).subscribe(
+      _ => {
+        this.router.navigate(['/tasks']);
+      },
+      _ => {
+        alert("Internal Server Error");
+      }
+    );
   }
 
 }
