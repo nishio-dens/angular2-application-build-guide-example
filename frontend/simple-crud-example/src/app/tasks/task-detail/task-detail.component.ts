80ad7f7bec51637ce4c2b02250abd25bd44e94bc
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-01 02:10:43 +0900

    [SimpleCrudExample] フォーム実装

--- frontend/simple-crud-example/src/app/tasks/task-detail/task-detail.component.ts
+++ frontend/simple-crud-example/src/app/tasks/task-detail/task-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-task-detail',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailComponent implements OnInit {
 
-  constructor() { }
+  taskForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.taskForm = this.formBuilder.group({
+      'name': ['', Validators.compose([Validators.required, Validators.maxLength(255)])],
+      'description': ['', Validators.maxLength(255)]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(values: any) {
+    console.log(values);
   }
 
 }
