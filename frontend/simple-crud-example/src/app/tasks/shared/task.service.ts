145f8c614c5a1bb3e75361b5a259ecb6ebd04c12
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-01 02:16:51 +0900

    [SimpleCrudExample] タスクを一通り更新できるようにサービス修正

--- frontend/simple-crud-example/src/app/tasks/shared/task.service.ts
+++ frontend/simple-crud-example/src/app/tasks/shared/task.service.ts
@@ -28,4 +28,33 @@ export class TaskService {
         );
       });
   }
+
+  getTask(id: number): Observable<Task> {
+    return this
+      .http
+      .get('api/tasks/' + id)
+      .map(r => r.json())
+      .map(t => new Task({
+          id: t['id'],
+          name: t['name'],
+          description: t['description'],
+          createdAt: t['created_at'],
+          updatedAt: t['updated_at']
+        })
+      );
+  }
+
+  createTask(task: Task): Observable<any> {
+    return this
+      .http
+      .post('api/tasks/', JSON.stringify(task))
+      .map(r => r.json());
+  }
+
+  updateTask(task: Task): Observable<any> {
+    return this
+      .http
+      .put('api/tasks/' + task.id, JSON.stringify(task))
+      .map(r => r.json());
+  }
 }
