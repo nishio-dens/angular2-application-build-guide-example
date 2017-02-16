62719e9d7c578a98ac065ca0135f4b3f52f818af
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-17 01:44:58 +0900

    remove not used devise options

--- backend/simple-auth-example/app/models/user.rb
+++ backend/simple-auth-example/app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+  devise :database_authenticatable, :registerable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # Relations
+
+  # Validations
+
+  # Methods
 end
