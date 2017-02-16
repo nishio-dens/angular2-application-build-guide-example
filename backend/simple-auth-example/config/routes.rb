d52a4b6547823b16da287a7d61536913e470b3b6
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-17 01:41:26 +0900

    change devise mount path

--- backend/simple-auth-example/config/routes.rb
+++ backend/simple-auth-example/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   root to: "health_check#index"
 
   namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+
     resources :tasks, only: [:index]
   end
 
