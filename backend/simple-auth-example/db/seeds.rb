bff33b6cd9d7437d9566320766ed3e84d2104f1b
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-17 01:49:30 +0900

    add example user seed

--- backend/simple-auth-example/db/seeds.rb
+++ backend/simple-auth-example/db/seeds.rb
@@ -14,3 +14,18 @@ tasks = [
 tasks.each do |t|
   Task.create(t) unless Task.exists?(t[:id])
 end
+
+users = [
+  {
+    id: 1,
+    provider: 'email',
+    password: 'password',
+    name: 'admin',
+    nickname: 'admin',
+    email: 'admin@densan-labs.net'
+  }
+]
+
+users.each do |t|
+  User.create(t) unless User.exists?(t[:id])
+end
