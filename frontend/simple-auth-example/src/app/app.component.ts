367df5c6bf03068d10c445fb01915ed2ab38fe0e
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-18 20:55:33 +0900

    add auth setting to app component

--- frontend/simple-auth-example/src/app/app.component.ts
+++ frontend/simple-auth-example/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Angular2TokenService} from 'angular2-token';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private tokenService: Angular2TokenService
+  ) {
+    // Authentication
+    this
+      .tokenService
+      .init({
+        apiBase: environment.apiBaseUrl,
+        signInPath: 'api/auth/sign_in',
+        signInRedirect: 'auth/login',
+        signOutPath: 'api/auth/sign_out',
+        signOutFailedValidate: true,
+        validateTokenPath: 'api/auth/validate_token',
+      });
+  }
 }
