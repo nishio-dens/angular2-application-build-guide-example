22bffd98aa67d861eccc3cc312a3c772588fdd11
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-18 20:31:15 +0900

    adding task detail

--- frontend/simple-auth-example/src/app/tasks/task-detail/task-detail.component.ts
+++ frontend/simple-auth-example/src/app/tasks/task-detail/task-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from '../shared/task.model';
+import {TaskService} from '../shared/task.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-task-detail',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-detail.component.css']
 })
 export class TaskDetailComponent implements OnInit {
+  task: Task = new Task();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.taskService.getTask(id).subscribe(
+        (t: Task) => this.task = t,
+        _ => {
+          alert("Record Not Found");
+          this.router.navigate(['/tasks']);
+        }
+      );
+    });
+  }
 }
