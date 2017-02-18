daf505fb14dd9ef73ba69ce2042b4243721e56c1
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-18 20:11:53 +0900

    adding routing / add task service

--- frontend/simple-auth-example/src/app/tasks/tasks-routing.module.ts
+++ frontend/simple-auth-example/src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TasksComponent} from './tasks.component';
+import {TaskListComponent} from './task-list/task-list.component';
+import {TaskDetailComponent} from './task-detail/task-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TasksComponent,
+    children: [
+      {
+        path: '',
+        component: TaskListComponent
+      },
+      {
+        path: 'tasks',
+        component: TaskListComponent
+      },
+      {
+        path: 'tasks/:id',
+        component: TaskDetailComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [
