dcce3b84f0692c19a19d741917da304afdb407ef
Shinsuke Nishio <nishio@densan-labs.net>
2017-02-18 20:14:36 +0900

    create task service and task model

--- frontend/simple-auth-example/src/app/tasks/shared/task.service.ts
+++ frontend/simple-auth-example/src/app/tasks/shared/task.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Task} from './task.model';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class TaskService {
 
-  constructor() { }
+  private baseUrl: string = environment.apiBaseUrl;
 
+  constructor(
+    private http: Http
+  ) { }
+
+  getTasks(): Observable<Task[]> {
+    return this
+      .http
+      .get(this.baseUrl + 'api/tasks')
+      .map(r => r.json())
+      .map(r => {
+        return r.map(t =>
+          new Task({
+            id: t['id'],
+            name: t['name'],
+            description: t['description'],
+            createdAt: t['created_at'],
+            updatedAt: t['updated_at']
+          })
+        );
+      });
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this
+      .http
+      .get(this.baseUrl + 'api/tasks/' + id)
+      .map(r => r.json())
+      .map(t => new Task({
+          id: t['id'],
+          name: t['name'],
+          description: t['description'],
+          createdAt: t['created_at'],
+          updatedAt: t['updated_at']
+        })
+      );
+  }
 }
